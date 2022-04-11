5e96dc8a1bd9f8d11c8a71cc83119fd62ea9d857
PavanKumarGitHub26 <pavanmkumar26@gmail.com>
2022-04-11 14:26:44 +0530

    manager dao

---
+++ src/main/java/com/project/newcarcare/dao/ManagerDao.java
@@ -0,0 +1,52 @@
+package com.project.newcarcare.dao;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.project.newcarcare.dto.Manager;
+import com.project.newcarcare.repository.ManagerRepository;
+
+
+@Repository
+public class ManagerDao {
+
+	@Autowired
+	private ManagerRepository managerRepository;
+
+	public Manager saveManager(Manager manager) {
+		return managerRepository.save(manager);
+	}
+
+	public Manager getManager(String id) {
+		Optional<Manager> optional = managerRepository.findById(id);
+		if (optional.isEmpty()) {
+			return null;
+
+		}
+
+		Manager manager = optional.get();
+
+		return manager;
+	}
+	
+	public boolean removeManager(String id) {
+		Manager manager=getManager(id);
+		if(manager!=null) {
+		managerRepository.delete(manager);
+		return true;
+		}
+		else
+			return false;
+	}
+	
+	public Manager updateManager(String id,Manager m1) {
+		Manager manager=getManager(id);
+		if(manager!=null)
+		{
+			return managerRepository.save(manager);
+		}
+		return null;
+	}
+}
