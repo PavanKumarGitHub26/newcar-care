14332409ca186828b9f9297a4a88ff491f98b217
Manojmadegowda <manojmadegowda@gmail.com>
2022-04-11 16:25:49 +0530

    car service repository

---
+++ src/main/java/com/project/newcarcare/repository/CarServiceRepository.java
@@ -0,0 +1,9 @@
+package com.project.newcarcare.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.newcarcare.dto.CarService;
+
+public interface CarServiceRepository extends JpaRepository<CarService, Integer> {
+
+}
