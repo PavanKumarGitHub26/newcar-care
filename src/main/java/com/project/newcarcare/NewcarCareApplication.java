d135ec0d9498b65cf2bf32cbcb2f3bf2cdbd1d54
PavanKumarGitHub26 <pavanmkumar26@gmail.com>
2022-04-11 12:24:58 +0530

    src

---
+++ src/main/java/com/project/newcarcare/NewcarCareApplication.java
@@ -0,0 +1,13 @@
+package com.project.newcarcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcarCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewcarCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/newcarcare/NewcarCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.newcarcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewcarCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
