ee67bec53151dbb62134825e3fbd4c439e69d035
Manojmadegowda <manojmadegowda@gmail.com>
2022-04-11 16:52:57 +0530

    copy database name

--- src/main/java/com/project/newcarcare/dto/CarService.java
+++ src/main/java/com/project/newcarcare/dto/CarService.java
@@ -1,5 +1,17 @@
 package com.project.newcarcare.dto;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+@Data
+@Entity
 public class CarService {
+@Id
+private int id;
+private String type;
+private double cost;
+private String duration;
+private String nisha;
 
 }
