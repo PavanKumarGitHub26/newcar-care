1853cbc174d59449dcdfffd347fb1650914c3f14
Manojmadegowda <manojmadegowda@gmail.com>
2022-04-12 17:30:08 +0530

    customer dao

--- src/main/java/com/project/newcarcare/dao/CustomerDao.java
+++ src/main/java/com/project/newcarcare/dao/CustomerDao.java
@@ -6,54 +6,59 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.project.newcarcare.dto.Branch;
 import com.project.newcarcare.dto.Customer;
 import com.project.newcarcare.repository.CustomerRepository;
 
 @Repository
 public class CustomerDao {
-	@Autowired 
+	@Autowired
 	CustomerRepository customerRepository;
-	
-	public Customer saveCustomer(Customer customer) {
-		return customerRepository.save(customer);
+
+	@Autowired
+	BranchDao branchDao;
+
+	public Customer saveCustomer(String id, Customer customer) {
+		Branch branch = branchDao.getBranch(id);
+		if (branch != null) {
+			customer.setBranch(branch);
+			return customerRepository.save(customer);
+
+		}
+
+		return null;
 	}
 
-	
 	public Customer getCustomerById(String id) {
 		Optional<Customer> opt = customerRepository.findById(id);
-		if(opt.isEmpty()) {
+		if (opt.isEmpty()) {
 			return null;
 		}
 		return opt.get();
 	}
-	
-	
-	
-	public List<Customer> getAllCustomer(){
-		return  customerRepository.findAll();
+
+	public List<Customer> getAllCustomer() {
+		return customerRepository.findAll();
 	}
-	
-	
+
 	public boolean deleteCustomer(String id) {
-	  Customer customer =getCustomerById(id);
-	  if(customer!=null) {
-		 customerRepository.delete(customer);
-		 return true;
-	  }
-	  return false;
-	 
+		Customer customer = getCustomerById(id);
+		if (customer != null) {
+			customerRepository.delete(customer);
+			return true;
+		}
+		return false;
+
 	}
-	
-	public Customer updateCustomer(String id,Customer customer) {
-		  Customer customer2 =getCustomerById(id);
-		  if(customer2!=null) {
-			  return customerRepository.save(customer);
-			
-		  }
-		  return null;
-		
+
+	public Customer updateCustomer(String id, Customer customer) {
+		Customer customer2 = getCustomerById(id);
+		if (customer2 != null) {
+			return customerRepository.save(customer);
+
+		}
+		return null;
+
 	}
-	
-	
 
 }
