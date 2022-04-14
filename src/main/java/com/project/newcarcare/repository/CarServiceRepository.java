c3e06a93c17c2ed79c0c7341cc51583d185da80c
Manojmadegowda <manojmadegowda@gmail.com>
2022-04-14 10:09:17 +0530

    carserviverepository

--- src/main/java/com/project/newcarcare/repository/CarServiceRepository.java
+++ src/main/java/com/project/newcarcare/repository/CarServiceRepository.java
@@ -1,9 +1,17 @@
 package com.project.newcarcare.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.project.newcarcare.dto.Car;
 import com.project.newcarcare.dto.CarService;
 
+
 public interface CarServiceRepository extends JpaRepository<CarService, Integer> {
+
+	@Query("select cs from CarService cs where cs.branch.id= :bid and cs.car.carNumber= :cnum")
+	public List<CarService> getAllCarService(String bid,String cnum);
 
 }
