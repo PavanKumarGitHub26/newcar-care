2c9e9afdb15380dfd2969fd7e3b33e04adaf4914
Manojmadegowda <manojmadegowda@gmail.com>
2022-04-14 10:16:31 +0530

    carservicedao

--- src/main/java/com/project/newcarcare/dao/CarServiceDao.java
+++ src/main/java/com/project/newcarcare/dao/CarServiceDao.java
@@ -9,27 +9,43 @@ import org.springframework.stereotype.Repository;
 import com.project.newcarcare.dto.Branch;
 import com.project.newcarcare.dto.Car;
 import com.project.newcarcare.dto.CarService;
+import com.project.newcarcare.dto.CarServiceAvailable;
+import com.project.newcarcare.dto.Customer;
 import com.project.newcarcare.repository.CarServiceRepository;
 
 @Repository
 public class CarServiceDao {
 	@Autowired
 	CarServiceRepository carServiceRepository;
-	
+
 	@Autowired
 	CarDao carDao;
-	
+
 	@Autowired
 	BranchDao branchDao;
-	
-	
 
-	public CarService saveCarService(String carNumber,String id  ,CarService carService) {
+	@Autowired
+	CarServiceAvailableDao carServiceAvailableDao;
+	
+	@Autowired
+	CarServiceDao carServiceDao;
+	
+	
+      public List<CarServiceAvailable>  AvailableService(){
+    	 
+    	  return carServiceAvailableDao.getAllCarServiceAvailable();
+    	
+    	  
+      }
+
+	public CarService saveCarService( int id, String carNumber, String bid, CarService carService) {
+		CarServiceAvailable carServiceAvailable = carServiceAvailableDao.getCarServiceAvailableById(id);
 		Car car = carDao.getCar(carNumber);
-		  Branch branch=branchDao.getBranch(id);
-		if(car!=null && branch!=null) {
+		Branch branch = branchDao.getBranch(bid);
+		if (carServiceAvailable!=null &&  car != null && branch != null) {
 			carService.setCar(car);
 			carService.setBranch(branch);
+			carService.setCarServiceAvailables((List<CarServiceAvailable>) carServiceAvailable);
 			return carServiceRepository.save(carService);
 		}
 		return null;
@@ -43,8 +59,9 @@ public class CarServiceDao {
 		return opt.get();
 	}
 
-	public List<CarService> getAllCarservice() {
-		return carServiceRepository.findAll();
+	public List<CarService> getAllCarservice(String bid, String cnum) {
+		return carServiceRepository.getAllCarService(bid, cnum);
+
 	}
 
 	public boolean deleteCarService(int id) {
@@ -55,16 +72,15 @@ public class CarServiceDao {
 		}
 		return false;
 	}
-	
-	public CarService updateCarService(int id,CarService carService) {
-		CarService carService1=getCarServiceById(id);
-		if(carService1!=null) {
-		 return	carServiceRepository.save(carService);
-			
+
+	public CarService updateCarService(int id, CarService carService) {
+		CarService carService1 = getCarServiceById(id);
+		if (carService1 != null) {
+			return carServiceRepository.save(carService);
+
 		}
 		return null;
-			
-		
+
 	}
 
 }
