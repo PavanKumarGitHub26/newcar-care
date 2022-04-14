ca2d336e89c48e7020cde6b4d13d04a4658bad9b
Manojmadegowda <manojmadegowda@gmail.com>
2022-04-14 10:11:09 +0530

    branch repository

--- src/main/java/com/project/newcarcare/repository/BranchRepository.java
+++ src/main/java/com/project/newcarcare/repository/BranchRepository.java
@@ -1,9 +1,14 @@
 package com.project.newcarcare.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.project.newcarcare.dto.Branch;
 
 public interface BranchRepository extends JpaRepository<Branch, String> {
 
+	@Query("select b from Branch b where b.manager.id=?1")
+	public List<Branch> getAll(String id) ;
 }
