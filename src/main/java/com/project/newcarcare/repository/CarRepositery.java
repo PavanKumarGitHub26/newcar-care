e190b4a898a924a2d1a9ddb278fa4de3db14f4a1
Manojmadegowda <manojmadegowda@gmail.com>
2022-04-14 10:10:13 +0530

    car repository

--- src/main/java/com/project/newcarcare/repository/CarRepositery.java
+++ src/main/java/com/project/newcarcare/repository/CarRepositery.java
@@ -1,9 +1,16 @@
 package com.project.newcarcare.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.project.newcarcare.dto.Branch;
 import com.project.newcarcare.dto.Car;
 
 public interface CarRepositery extends JpaRepository<Car, String> {
+
+	@Query("select c from Car c where c.customer.id=?1")
+	public  List<Car> getAllCars(String id);
 
 }
