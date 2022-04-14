4a4469069f84c861b79d12afb2d234b3cfb75ac8
Manojmadegowda <manojmadegowda@gmail.com>
2022-04-14 10:08:06 +0530

    customer repository

--- src/main/java/com/project/newcarcare/repository/CustomerRepository.java
+++ src/main/java/com/project/newcarcare/repository/CustomerRepository.java
@@ -1,9 +1,15 @@
 package com.project.newcarcare.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.project.newcarcare.dto.Branch;
 import com.project.newcarcare.dto.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, String> {
+	@Query("select c from Customer c where c.branch.id=?1")
+	public List<Customer> getAllCustomer(String id) ;
 
 }
