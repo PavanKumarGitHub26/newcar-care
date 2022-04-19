ba80306ccafb8eafa4feb7afdc9d699f7dca3793
PavanKumarGitHub26 <pavanmkumar26@gmail.com>
2022-04-19 09:49:59 +0530

    manager controller

--- src/main/java/com/project/newcarcare/Controller/ManagerController.java
+++ src/main/java/com/project/newcarcare/Controller/ManagerController.java
@@ -16,43 +16,68 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.newcarcare.Service.ManagerService;
 import com.project.newcarcare.dto.Manager;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 public class ManagerController {
 	@Autowired
 	ManagerService managerService;
-	
+
+	@ApiOperation(value = "save the manager details", produces = "application/json", consumes = "application/json")
+	@ApiResponses({ @ApiResponse(code = 200, message = "manager saved"),
+			@ApiResponse(code = 405, message = "bad request,not proper car data") })
 
 	@PostMapping("/manager")
-	public Manager saveManager(@RequestBody  Manager manager) {
+	public Manager saveManager(@RequestBody Manager manager) {
 		return managerService.saveManager(manager);
 	}
-	
+
+	@ApiOperation(value = "get the manager details", produces = "application/json", consumes = "application/json")
+	@ApiResponses({ @ApiResponse(code = 200, message = "get manager"),
+			@ApiResponse(code = 405, message = "bad request,not proper car data") })
+
 	@GetMapping("/manager")
 	public Manager getManagerById(@RequestParam String id) {
 		return managerService.getManager(id);
 	}
-	
+
+	@ApiOperation(value = "get all manager details", produces = "application/json", consumes = "application/json")
+	@ApiResponses({ @ApiResponse(code = 200, message = "get all manager"),
+			@ApiResponse(code = 405, message = "bad request,not proper car data") })
+
 	@GetMapping("/allmanager")
-	public List<Manager> getAllManager(){
+	public List<Manager> getAllManager() {
 		return managerService.getAllManager();
 	}
-	
-	
+
+	@ApiOperation(value = "delete the manager", produces = "application/json", consumes = "application/json")
+	@ApiResponses({ @ApiResponse(code = 200, message = "manager deleted"),
+			@ApiResponse(code = 405, message = "bad request,not proper car data") })
+
 	@DeleteMapping("/manager")
-	public void deleteHospital(String id) {
+	public void deleteManager(String id) {
 		managerService.removeManager(id);
 	}
-	
+
+	@ApiOperation(value = "update the manager details", produces = "application/json", consumes = "application/json")
+	@ApiResponses({ @ApiResponse(code = 200, message = "manager updated"),
+			@ApiResponse(code = 405, message = "bad request,not proper car data") })
+
 	@PutMapping("/manager")
 	public Manager updateManagerById(@RequestParam String id, @RequestBody Manager manager) {
 		return managerService.updateManager(id, manager);
 	}
-	
+
+	@ApiOperation(value = "to get the body ", produces = "application/json", consumes = "application/json")
+	@ApiResponses({ @ApiResponse(code = 200, message = "body obtained"),
+			@ApiResponse(code = 405, message = "bad request,not proper car data") })
+
 
 	@GetMapping("/hii")
 	public Manager get() {
 		return new Manager();
 	}
-	
 
 }
