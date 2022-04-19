fd578b6708ae0921a17d29b183b0bd29920d5aa4
PavanKumarGitHub26 <pavanmkumar26@gmail.com>
2022-04-19 09:52:13 +0530

    customer dao

--- src/main/java/com/project/newcarcare/dao/CustomerDao.java
+++ src/main/java/com/project/newcarcare/dao/CustomerDao.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.newcarcare.dto.Branch;
 import com.project.newcarcare.dto.Customer;
@@ -14,24 +16,19 @@ import com.project.newcarcare.repository.CustomerRepository;
 public class CustomerDao {
 	@Autowired
 	CustomerRepository customerRepository;
-
 	@Autowired
 	BranchDao branchDao;
 
-	public Customer saveCustomer(String id, Customer customer) {
-		Branch branch = branchDao.getBranch(id);
-		if (branch != null) {
-			customer.setBranch(branch);
+	
+
+	public Customer saveCustomer(@ RequestParam String id, @RequestBody Customer customer) {
 			return customerRepository.save(customer);
 
 		}
 
-		return null;
-	}
-
 	public Customer getCustomerById(String id) {
 		Optional<Customer> opt = customerRepository.findById(id);
-		if (opt.isEmpty()) {
+		if (opt==null) {
 			return null;
 		}
 		return opt.get();
